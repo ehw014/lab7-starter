32f21b861b68ae6f63a8465ebfebb1caf7ba93a1
Evan Wu <35472009+ehw014@users.noreply.github.com>
2024-05-21 17:58:03 -0700

    Update RecipeCard.js for pages try 2

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -131,7 +131,7 @@ class RecipeCard extends HTMLElement {
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
 		artiEle.innerHTML = `
-		<img src=".${data.imgSrc}"
+		<img src="${data.imgSrc}"
 		alt="${data.imgAlt}">
 		<p class="title">
 			<a href="${data.titleLnk}">${data.titleTxt}</a>
@@ -139,7 +139,7 @@ class RecipeCard extends HTMLElement {
 		<p class="organization">${data.organization}</p>
 		<div class="rating">
 			<span>${data.rating}</span>
-				<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+				<img src="./assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
 			<span>(${data.numRatings})</span>
 		</div>
 		<time>${data.lengthTime}</time>
